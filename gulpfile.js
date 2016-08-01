a3a48e95b30068004459ca15e99c382b6a6befa9
Katie Golicz <goliczk@gmail.com>
2016-08-01 13:39:08 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+// Gulpfile
+
+var gulp = require('gulp');
+var karma = require('karma').Server;
+gulp.task('test', function(done){
+new karma({
+configFile: __dirname + '/karma.conf.js'
+}, done).start();
+});
